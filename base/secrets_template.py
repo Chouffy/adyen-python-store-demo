6eb4dbd231733e05b609d4035316239f7917ed75
Chouffy <git@chouffy.net>
2020-04-29 18:09:49 +0200

    Initial commit

---
+++ base/secrets_template.py
@@ -0,0 +1,12 @@
+# Define here all secrets you don't want to publish
+
+
+def secrets():
+    return {
+        'platform': "test",
+        'app_name': "appName",
+        'returnUrl': "http://...",
+        'merchantAccount': "...",
+        'xapikey': '...',
+        'originKey': '...',
+    }
